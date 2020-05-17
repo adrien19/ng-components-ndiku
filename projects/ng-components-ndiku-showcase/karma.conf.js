84275ff44a9f28815e779fa28abd35fd868e02bc
Adrien <n.adrien@mun.ca>
2020-05-17 15:47:24 -0230

    chores(karma): removes puppeteer  as a devDependency

--- projects/ng-components-ndiku-showcase/karma.conf.js
+++ projects/ng-components-ndiku-showcase/karma.conf.js
@@ -34,16 +34,16 @@ module.exports = function (config) {
   });
 };
 
-module.exports = function (config) {
-  config.set({
-    browsers: ["Chrome", "Chrome_without_security"], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
+// module.exports = function (config) {
+//   config.set({
+//     browsers: ["Chrome", "Chrome_without_security"], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
 
-    // you can define custom flags
-    customLaunchers: {
-      Chrome_without_security: {
-        base: "Chrome",
-        flags: ["--disable-web-security", "--disable-site-isolation-trials"],
-      },
-    },
-  });
-};
+//     // you can define custom flags
+//     customLaunchers: {
+//       Chrome_without_security: {
+//         base: "Chrome",
+//         flags: ["--disable-web-security", "--disable-site-isolation-trials"],
+//       },
+//     },
+//   });
+// };
