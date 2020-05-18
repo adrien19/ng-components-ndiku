7447396da0ae75f41aad49986a63e69f18d0ee63
Adrien <n.adrien@mun.ca>
2020-05-17 22:14:03 -0230

    chores(lib): adds browserModule to lib module

--- projects/ng-components-ndiku/src/lib/ng-components-ndiku.module.ts
+++ projects/ng-components-ndiku/src/lib/ng-components-ndiku.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [NgComponentsNdikuComponent],
   imports: [
+    BrowserModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
