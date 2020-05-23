84a7650284a30e126903c5de214afb16b6850e2a
Adrien <n.adrien@mun.ca>
2020-05-23 17:27:39 -0230

    fix(app-table): fixes the mismatch types in fake data file

--- projects/ng-components-ndiku-showcase/src/app/table-data/fake-data.ts
+++ projects/ng-components-ndiku-showcase/src/app/table-data/fake-data.ts
@@ -4,21 +4,21 @@ export const PROJECTS: Project[] = [
     id: 1,
     name: 'Mercury',
     cost: 277000000,
-    first_flight: 'September 9, 1959',
+    FIRST_FLIGHT: 'September 9, 1959',
     status: 'Complete',
   },
   {
     id: 2,
     name: 'Gemini',
     cost: 1300000000,
-    first_flight: 'April 8, 1964',
+    FIRST_FLIGHT: 'April 8, 1964',
     status: 'Complete',
   },
   {
     id: 3,
     name: 'Apollo',
     cost: 25400000000,
-    first_flight: 'February 26, 1966',
+    FIRST_FLIGHT: 'February 26, 1966',
     status: 'Complete',
   },
   {
@@ -36,8 +36,8 @@ export const PROJECTS: Project[] = [
   {
     id: 6,
     name: 'Space Shuttle',
-    total_cost: 196000000000,
-    first_flight: 'August 12, 1977',
+    TOTAL_COST: 196000000000,
+    FIRST_FLIGHT: 'August 12, 1977',
     status: 'Complete',
   },
 ];
@@ -46,7 +46,7 @@ export const PERSONNEL: Person[] = [
     id: 151,
     name: 'Alan B. Shepard, Jr.',
     job: 'Astronaut',
-    year_joined: 1959,
+    YEAR_JOINED: 1959,
     missions: ['MR-3', 'Apollo 14'],
     crewWith: [
       {
@@ -63,28 +63,28 @@ export const PERSONNEL: Person[] = [
     id: 152,
     name: 'Virgil I. Grissom',
     job: 'Astronaut',
-    year_joined: 1959,
+    YEAR_JOINED: 1959,
     missions: ['MR-4', 'Apollo 1'],
   },
   {
     id: 153,
     name: 'John H. Glenn, Jr.',
     job: 'Astronaut',
-    year_joined: 1959,
+    YEAR_JOINED: 1959,
     missions: ['MA-6', 'STS-95'],
   },
   {
     id: 154,
     name: 'M. Scott Carpenter',
     job: 'Astronaut',
-    year_joined: 1959,
+    YEAR_JOINED: 1959,
     missions: ['MA-7'],
   },
   {
     id: 161,
     name: 'James A. Lovell',
     job: 'Astronaut',
-    year_joined: 1962,
+    YEAR_JOINED: 1962,
     missions: ['Gemini 7', 'Gemini 12', 'Apollo 8', 'Apollo 13'],
     manager: {
       id: 157,
